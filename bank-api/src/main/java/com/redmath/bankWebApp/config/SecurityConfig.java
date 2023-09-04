586c05080bf03d5d11b66bbdb2036dd2c1cc49da
AtifButt123 <bsef19m001@pucit.edu.pk>
2023-09-04 05:08:53 +0500

    bank app frontend setup

---
+++ bank-api/src/main/java/com/redmath/bankWebApp/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.redmath.bankWebApp.config;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+@EnableMethodSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+
+        return web -> web.ignoring().requestMatchers(
+                new AntPathRequestMatcher("/", "GET"),
+                new AntPathRequestMatcher("/actuator", "GET"),
+                new AntPathRequestMatcher("/actuator/**","GET")
+//                new AntPathRequestMatcher("/api/v1/accounts/**","GET"),
+//                new AntPathRequestMatcher("/api/v1/accounts/**","POST")
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin(Customizer.withDefaults());
+//        http.exceptionHandling(config -> config.defaultAuthenticationEntryPointFor(
+//                (request, response, ex) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Not Authorized"),
+//                AntPathRequestMatcher.antMatcher(api)));
+
+        http.csrf(config -> config.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()));
+
+        http.authorizeHttpRequests(config -> config.anyRequest().authenticated());
+
+//        Disable CSRF
+//        http.csrf(csrf -> csrf.disable());
+
+
+
+        return http.build();
+    }
+
+}
